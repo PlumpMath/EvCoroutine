18ba298f416460beaa4251fd72cb7419f8612819
Eduardo Ventanilha <eventanilha@gmail.com>
2017-01-13 05:07:26 -0200

    Add project files.

---
+++ EvCoroutine/CoroutineManager.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace EvCoroutine
+{
+    public class CoroutineManager
+    {
+        private static List<Coroutine> coroutines = new List<Coroutine>();
+
+        public static Coroutine StartCoroutine(IEnumerator routine)
+        {
+            var coroutine = new Coroutine(routine);
+
+            coroutines.Add(coroutine);
+
+            return coroutine;
+        }
+
+        public static void Update()
+        {
+            for (int i = 0; i < coroutines.Count; i++)
+            {
+                var coroutine = coroutines[i];
+
+                if (!coroutine.MoveNext())
+                {
+                    coroutines[i] = coroutines[coroutines.Count - 1];
+                    coroutines.RemoveAt(coroutines.Count - 1);
+                }
+            }
+        }
+    }
+
+    public class YieldInstruction
+    {
+        internal IEnumerator routine;
+
+        internal YieldInstruction() { }
+
+        internal bool MoveNext()
+        {
+            var yieldInstruction = routine.Current as YieldInstruction;
+
+            if (yieldInstruction != null)
+            {
+                if (yieldInstruction.MoveNext())
+                {
+                    return true;
+                }
+                else if (routine.MoveNext())
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else if (routine.MoveNext())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+    public class Coroutine : YieldInstruction
+    {
+        public Coroutine(IEnumerator routine)
+        {
+            this.routine = routine;
+        }
+    }
+
+    public class WaitForSeconds : YieldInstruction
+    {
+        public WaitForSeconds(float seconds)
+        {
+            var delay = DateTime.Now.AddSeconds(seconds);
+            this.routine = Count(delay);
+        }
+
+        private IEnumerator Count(DateTime delay)
+        {
+            while (DateTime.Now <= delay)
+                yield return true;
+        }
+    }
+}
---
+++ EvCoroutine/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+
+namespace EvCoroutine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CoroutineManager.StartCoroutine(Cor1());
+            CoroutineManager.StartCoroutine(Cor2());
+            CoroutineManager.StartCoroutine(Cor3());
+
+            CoroutineManager.StartCoroutine(Cor4());
+
+            var lastUpdate = DateTime.Now.Ticks;
+
+            while (true)
+            {
+                if (IsKeyEscapePress())
+                    return;
+
+                var currentUpdate = DateTime.Now.Ticks;
+                var deltaTime = (float)TimeSpan.FromTicks(currentUpdate - lastUpdate).TotalMilliseconds;
+
+                CoroutineManager.Update();
+
+                lastUpdate = currentUpdate;
+            }
+        }
+        static bool IsKeyEscapePress()
+        {
+            return (Console.KeyAvailable && Console.ReadKey(true).Key == ConsoleKey.Escape);
+        }
+
+        
+        static IEnumerator Cor1()
+        {
+            Console.WriteLine("Hello Cor1-in " + DateTime.Now);
+            yield return new WaitForSeconds(7f);
+            Console.WriteLine("Hello Cor1-out " + DateTime.Now);
+            yield break;
+        }
+
+        static IEnumerator Cor2()
+        {
+            Console.WriteLine("Hello Cor2-in " + DateTime.Now);
+            yield return new WaitForSeconds(3f);
+            Console.WriteLine("Hello Cor2-out " + DateTime.Now);
+            yield break;
+        }
+
+        static IEnumerator Cor3()
+        {
+            Console.WriteLine("Hello Cor3-in " + DateTime.Now);
+            yield return new WaitForSeconds(5f);
+            Console.WriteLine("Hello Cor3-out " + DateTime.Now);
+            yield break;
+        }
+
+
+        static IEnumerator Cor4()
+        {
+            Console.WriteLine("Hello Cor4-in " + DateTime.Now);
+            yield return CoroutineManager.StartCoroutine(Cor5(6f));
+            Console.WriteLine("Hello Cor4-out " + DateTime.Now);
+        }
+
+        static IEnumerator Cor5(float wait)
+        {
+            Console.WriteLine("Hello Cor5-in  " + DateTime.Now);
+            yield return new WaitForSeconds(wait);
+            Console.WriteLine("Hello Cor5-out " + DateTime.Now);
+        }
+    }
+}
